ace4dc8a1e9e783f397db120dcc703422b81988b
ilavisharma <lavisharma796@gmail.com>
2019-05-08 16:53:56 +0530

    completed

--- Day 2 - Css + JS clock/app.js
+++ Day 2 - Css + JS clock/app.js
@@ -1,1 +1,24 @@
-alert('hi');
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  //   second hand
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  //   minute hand
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  //   minute hand
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 60) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
