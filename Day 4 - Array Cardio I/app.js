69610fa0f24962f3a2f830817e5c778306d45967
ilavisharma <lavisharma796@gmail.com>
2019-05-14 22:37:51 +0530

    sort second

--- Day 4 - Array Cardio I/app.js
+++ Day 4 - Array Cardio I/app.js
@@ -98,15 +98,22 @@ const oldest = inventors.sort((first, second) =>
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
-const category = document.querySelector('.mw-category');
-const links = category.querySelectorAll('a');
+// const category = document.querySelector('.mw-category');
+// const links = category.querySelectorAll('a');
 
-const de = links
-  .map(link => link.textContent)
-  .filter(streetName => streetName.includes('de'));
+// const de = links
+//   .map(link => link.textContent)
+//   .filter(streetName => streetName.includes('de'));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+
+const alpha = people.sort((last, next) => {
+  const [aLast, aFirst] = last.split(', ');
+  const [bLast, bFirst] = next.split(', ');
+  return aLast > bLast ? -1 : 1;
+});
+console.log(alpha);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
